5b444527cbca04f8f5a1bf8fb155deb228be0096
Dagan Hartmann <110746303+ch3mbot@users.noreply.github.com>
2024-05-23 21:21:28 -0400

    Update index.js
    added support for new site

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var button = document.getElementById("button-math");
-//button.addEventListener("click", OpenLink("Math-Practice"));
-button.addEventListener("click", function() { OpenLink("math-practice"); } );
+var button_math = document.getElementById("button-math");
+var button_quant = document.getElementById("button-makecode-quantizer");
+
+button_math.addEventListener("click", function() { OpenLink("math-practice"); } );
+button_quant.addEventListener("click", function() { OpenLink("MakeCodeImageQuantizerJS"); } );
 
 function OpenLink(subpage) {
     window.open("https://ch3mbot.github.io/" + subpage, "_self");
