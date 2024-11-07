9c393f0fa9a53197155437f140e063b4c16944dd
Dagan Hartmann <dagan.hartmann@gmail.com>
2024-11-07 14:50:33 -0500

    fixed formatting and added new button
    added unimplemented button to new webpage

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var button_math = document.getElementById("button-math");
 var button_quant = document.getElementById("button-makecode-quantizer");
+var button_unitystuff = document.getElementById("button-unity-info-pages")
 
 button_math.addEventListener("click", function() { OpenLink("math-practice"); } );
 button_quant.addEventListener("click", function() { OpenLink("MakeCodeImageQuantizerJS"); } );
+button_unitystuff.addEventListener("click", function() { OpenLink("unity-info-pages"); } );
 
 function OpenLink(subpage) {
     window.open("https://ch3mbot.github.io/" + subpage, "_self");
